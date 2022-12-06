94b80fdb6eaf637454999abb0a690cde765e0b36
slgeay <avomailbox-github@yahoo.fr>
2022-12-06 10:22:00 +0100

    Shorten concat

--- app/day06.py
+++ app/day06.py
@@ -3,7 +3,7 @@ import re
 
 def day06(s: str, n: int):
     # Constructs and uses a "(.)(?!\1)(.)(?!\1|\2)(.)"... regex
-    print(re.search("(.)"+ "".join(["(?!\\" + "|\\".join([str(j + 1) for j in range(i+1)]) + ")(.)" for i in range(n-1)]),s,).end())
+    print(re.search("".join(["(.)(?!\\"+"|\\".join([str(j+1) for j in range(i+1)])+")" for i in range(n-1)])+".",s).end())
 
 
 def day06a(lines: list[str]):

387f4a83e48b6ebc1da0d65445d6772098e59ab6
slgeay <avomailbox-github@yahoo.fr>
2022-12-20 14:11:56 +0100

    Optim Day 20

--- app/day20.py
+++ app/day20.py
@@ -1,12 +1,12 @@
 def day20a_golf(l: list[str]) -> str:
-    return str((l:=(n:=len(l))and(L:=[int(i)for i in l])and(I:=[i for i in range(n)])and[(L:=(m:=L[:(i:=I.index(x))]+L[i+1:])[:(j:=(i+(a:=L[i]))%(n-1))]+[a]+m[j:],I:=(I:=I[:i]+I[i+1:])[:j]+[x]+I[j:])for x in range(n)][-1][0])[(1000+(z:=l.index(0)))%len(l)]+l[(2000+z)%len(l)]+l[(3000+z)%len(l)])
+    return(n:=len(l:=[int(i)for i in l]))and(I:=list(range(n)))and[I.insert(((i:=I.index(j))+l[j])%(n-1),I.pop(i))for j in range(n)]and str(sum((L:=[l[i]for i in I])[(x*1000+L.index(0))%n]for x in[1,2,3]))
 
 
 def day20b_golf(l: list[str]) -> str:
-    return str((l:=(n:=len(l))and(L:=[811589153*int(i)for i in l])and(I:=[i for i in range(n)])and[(L:=(m:=L[:(i:=I.index(x))]+L[i+1:])[:(j:=(i+(a:=L[i]))%(n-1))]+[a]+m[j:],I:=(I:=I[:i]+I[i+1:])[:j]+[x]+I[j:])for _ in range(10)for x in range(n)][-1][0])[(1000+(z:=l.index(0)))%len(l)]+l[(2000+z)%len(l)]+l[(3000+z)%len(l)])
+    return(n:=len(l:=[811589153*int(i)for i in l]))and(I:=list(range(n)))and[I.insert(((i:=I.index(j))+l[j])%(n-1),I.pop(i))for _ in range(10)for j in range(n)]and str(sum((L:=[l[i]for i in I])[(x*1000+L.index(0))%n]for x in[1,2,3]))
 
 
-### This is the original code, quicker than the golfed version
+### First code
 def day20(l: list[str], r=1, f=1) -> str:
     n = len(l)
     l = [int(i) * f for i in l]
@@ -15,13 +15,11 @@ def day20(l: list[str], r=1, f=1) -> str:
     for _ in range(r):
         for ii in range(n):
             i = indexes.index(ii)
-            a = l.pop(i)
-            j = (i + a) % (n - 1)
+            j = (i + l[ii]) % (n - 1)
             indexes.pop(i)
-            l.insert(j, a)
             indexes.insert(j, ii)
 
-    return str(l[(1000 + (z := l.index(0))) % len(l)] + l[(2000 + z) % len(l)] + l[(3000 + z) % len(l)])
+    return str((l:=[l[i] for i in indexes])[(1000 + (z := l.index(0))) % len(l)] + l[(2000 + z) % len(l)] + l[(3000 + z) % len(l)])
 
 
 def day20a(lines: list[str]) -> str:
@@ -31,4 +29,4 @@ def day20a(lines: list[str]) -> str:
 
 def day20b(lines: list[str]) -> str:
     return day20b_golf(lines)
-    # return day20(lines, 10, 811589153)
+    #return day20(lines, 10, 811589153)
